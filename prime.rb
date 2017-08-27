9dfb5e03f5552678ac180daff613a6417c050078
Alexander Findlay <info@coded.media>
2017-08-27 10:27:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2
+  count = 2
+  while number > count
+    return false if number % count == 0
+    count += 1
+  end
+  return true
+end
